0c69ca288055997cd455dc223c52044675e8f605
Kirisan Thiruchelvam <73609371+kkpro18@users.noreply.github.com>
2024-09-23 23:44:17 +0100

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Main extends frame{
+
+    private Button[] button_numbers;
+    private Button add, subtract, divide, multiply, equals;
+    private TextField result_window;
+
+    public calculator(){
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
